46489db862a1a693767f920497e06854d2d9b7b3
APKonovalov <APKonovalov@alfabank.ru>
2023-09-18 21:50:06 +0300

    [01_web] task 1*

--- 01_web/http-server/src/main/java/ru/netology/Main.java
+++ 01_web/http-server/src/main/java/ru/netology/Main.java
@@ -1,8 +1,33 @@
 package ru.netology;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
 // https://github.com/netology-code/jspr-homeworks/tree/master/01_web
 public class Main {
     public static void main(String[] args) {
+        Server.addHandler("GET", "/classic.html", (request, out) -> {
+                    final var filePath = Path.of(".", "public", request.getPath());
+                    final var mimeType = Files.probeContentType(filePath);
+
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                }
+        );
+
         Server.listen(9999);
     }
 }
--- 01_web/http-server/src/main/java/ru/netology/Server.java
+++ 01_web/http-server/src/main/java/ru/netology/Server.java
@@ -3,16 +3,21 @@ package ru.netology;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class Server {
 
+    static final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    static final Executor executor = Executors.newFixedThreadPool(64);
+
+    static ConcurrentMap<ConcurrentMap<String, String>, Handler> requestHandlers = new ConcurrentHashMap<>();
+
     private Server() {
     }
-
-    static final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-    static final Executor executor = Executors.newFixedThreadPool(64);
 
     public static void listen(int port) {
         try (final var serverSocket = new ServerSocket(port)) {
@@ -24,5 +29,11 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void addHandler(String method, String path, Handler handler) {
+        ConcurrentMap<String, String> key = new ConcurrentHashMap<>();
+        key.put(method, path);
+        requestHandlers.put(key, handler);
     }
 }
