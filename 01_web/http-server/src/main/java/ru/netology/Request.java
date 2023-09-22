79a19a4cc8288a27ab1b76950a158e24bb1df8d3
APKonovalov <APKonovalov@alfabank.ru>
2023-09-22 22:34:04 +0300

    [02_forms] task 1 - fix after review

--- 01_web/http-server/src/main/java/ru/netology/Request.java
+++ 01_web/http-server/src/main/java/ru/netology/Request.java
@@ -6,12 +6,15 @@ import org.apache.http.client.utils.URLEncodedUtils;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Optional;
 
 public class Request {
 
     private final String method;
 
     private final URI uri;
+
+    private final List<NameValuePair> queryParams;
 
     public URI getUri() {
         return uri;
@@ -24,9 +27,17 @@ public class Request {
     public Request(String method, URI uri) {
         this.method = method;
         this.uri = uri;
+        this.queryParams = URLEncodedUtils.parse(uri, StandardCharsets.UTF_8);
     }
 
     public List<NameValuePair> getQueryParams() {
-        return URLEncodedUtils.parse(uri, StandardCharsets.UTF_8);
+        return queryParams;
+    }
+
+    public String getQueryParam(String name) {
+        Optional<NameValuePair> foundedPair = queryParams.stream()
+                .filter(nameValuePair -> nameValuePair.getName().equals(name))
+                .findFirst();
+        return foundedPair.map(NameValuePair::getValue).orElse(null);
     }
 }
