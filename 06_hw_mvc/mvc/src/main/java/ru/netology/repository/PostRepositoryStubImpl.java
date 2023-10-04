e92e2d437f3956555453c3f60ecccf47c859e575
APKonovalov <APKonovalov@alfabank.ru>
2023-10-04 23:22:18 +0300

    [06_mvc] task 1 - migration

---
+++ 06_hw_mvc/mvc/src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -0,0 +1,51 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class PostRepositoryStubImpl implements PostRepository {
+    private static final AtomicInteger currentId = new AtomicInteger(0);
+
+    private final List<Post> posts = Collections.synchronizedList(new ArrayList<>());
+
+    public List<Post> all() {
+        return posts;
+    }
+
+    public Optional<Post> getById(int id) {
+        return posts.stream().filter(post -> post.getId() == id)
+                .findFirst();
+    }
+
+    public Post save(Post post) throws IllegalArgumentException {
+        if (post.getId() == 0) {
+            Post newPost = new Post(currentId.incrementAndGet(), post.getContent());
+            posts.add(newPost);
+            return newPost;
+        }
+        Optional<Post> postToBeUpdated = getById(post.getId());
+        if (!postToBeUpdated.isPresent()) {
+            throw new IllegalArgumentException("Can't update post. Post with id '" + post.getId() + "' not found in store.");
+        }
+        Post updatedPost = postToBeUpdated.get();
+        posts.remove(updatedPost);
+        updatedPost.setContent(post.getContent());
+        posts.add(updatedPost);
+        return updatedPost;
+    }
+
+    public void removeById(int id) throws IllegalArgumentException {
+        Optional<Post> postToBeRemoved = getById(id);
+        if (!postToBeRemoved.isPresent()) {
+            throw new IllegalArgumentException("Can't remove post. Post with id '" + id + "' not found in store.");
+        }
+        posts.remove(postToBeRemoved.get());
+    }
+}
